02132283224a2af40925988730dbf85e6e8f2ddf
bryan <kemzangbryan@gmail.com>
2025-08-15 04:39:34 +0200

    Initialisation du projet GesProd

---
+++ src/test/java/store/gesprod/GesprodApplicationTests.java
@@ -0,0 +1,13 @@
+package store.gesprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesprodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
