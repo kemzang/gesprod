35fa326347e40c649ba3a701009950d1c17e2722
bryan <kemzangbryan@gmail.com>
2025-08-15 10:13:43 +0200

    feature/06-swagger

---
+++ src/main/java/store/gesprod/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package store.gesprod.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI gesprodOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("GESPROD API")
+                        .contact(new Contact()
+                                .email("kwenol@yahoo.fr")
+                                .name("Store Enterprise")
+                                .url("https://store.cm"))
+                        .description("API de gestion des produits et catégories")
+                        .version("1.0"));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+    }
+}
